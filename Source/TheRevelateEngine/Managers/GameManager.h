de728073d1932d59d0437472e5666d481a61110e
Avalon Brathwaite <virlinthejammer@gmail.com>
2025-09-24 00:25:54 +0100

    Added game manager and changed project settings

---
+++ Source/TheRevelateEngine/Managers/GameManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEREVELATEENGINE_API UGameManager : public UObject
+{
+	GENERATED_BODY()
+};
