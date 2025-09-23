1552be746272cdc39fcd8771ef7af8f665af617e
Avalon Brathwaite <virlinthejammer@gmail.com>
2025-09-24 00:26:49 +0100

    U asset changes

--- Source/TheRevelateEngine/Managers/GameManager.cpp
+++ Source/TheRevelateEngine/Managers/GameManager.cpp
@@ -1,4 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// 2025, The Revelate Engine
 
 
 #include "GameManager.h"
+
+void AGameManager::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentState = EGameState::DayOne;
+	FString EnumName = UEnum::GetValueAsString(CurrentState);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, EnumName);
+
+	OnGameStateChanged.AddUniqueDynamic(this,&AGameManager::HandleGameStateChange); 
+
+}
+
+EGameState AGameManager::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void AGameManager::HandleGameStateChange(EGameState OldState)
+{
+	// Here we need to set stuff up to change the game state on next time opening the game as we don't want it to change straight away,
+	// maybe a bool that becomes true.
+	// Handle save stuff here maybe? The HUD can change and deactivate from the task manager but we could do a separate call to the UI here.
+}
+
+// The task manager can get the current state and map the correct tasks to display.
+// The task manager should handle what tasks are completed and handle if a task has a specific flag for an event.
+// The Game manager should handle all core gameplay changes like crashing the game, api stuff, ect.
+// Once all tasks are done, it can send a delegate to the game manager to handle the clock out functionality.
+
+// If day one, tasks are x y z, can use some sort of look up table for this. 
+// the HUD could also be bound to the delegate fired from the game manager, this could be fired at the
+// start of the game, so all managers and other bits know what state they're in and can choose the correct HUD accordingly. 
--- Source/TheRevelateEngine/Managers/GameManager.h
+++ Source/TheRevelateEngine/Managers/GameManager.h
@@ -1,16 +1,40 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// 2025, The Revelate Engine 
 #pragma once
-
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "UObject/Object.h"
 #include "GameManager.generated.h"
 
 /**
- * 
+ * Add explanation here queen 
  */
-UCLASS()
-class THEREVELATEENGINE_API UGameManager : public UObject
+
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	DayOne      UMETA(DisplayName = "Day One"),
+	DayTwo       UMETA(DisplayName = "Day Two"),
+	DayThree       UMETA(DisplayName = "Day Three"),
+	DayFour       UMETA(DisplayName = "Day Four"),
+	DayFive       UMETA(DisplayName = "Day Five"),
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewState);
+
+UCLASS(BlueprintType, Blueprintable)
+class THEREVELATEENGINE_API AGameManager : public AActor
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable) EGameState GetCurrentState() const;
+	UFUNCTION() void HandleGameStateChange(EGameState OldState);
+
+	UPROPERTY() EGameState CurrentState;
+	UPROPERTY(BlueprintReadWrite) FOnGameStateChanged OnGameStateChanged;
+
+protected:
+	virtual void BeginPlay() override;
+private:
+	
 };
