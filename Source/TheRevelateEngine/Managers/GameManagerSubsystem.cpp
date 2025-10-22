a4ec1b6d6c387ee54f7b230c0cd1e6e2a9e5ea8e
Avalon Brathwaite <virlinthejammer@gmail.com>
2025-10-22 01:38:36 +0100

    Working on making save file for current state and bool if tasks were completed by player. Began working on a potential GameInstanceSubsystem to handle game states and saving/loading files as this would avoid needing the gamemanager to be an actor and avoid using gameinstance at all. Centralized!!

---
+++ Source/TheRevelateEngine/Managers/GameManagerSubsystem.cpp
@@ -0,0 +1,24 @@
+// 2025 The Revelate Engine
+
+
+#include "GameManagerSubsystem.h"
+
+void UGameManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UGameManagerSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+EGameState UGameManagerSubsystem::GetCurrentState() const
+{
+	
+}
+
+void UGameManagerSubsystem::HandleGameStateChange(EGameState OldState)
+{
+	
+}
---
+++ Source/TheRevelateEngine/Managers/GameManagerSubsystem.h
@@ -0,0 +1,36 @@
+// 2025 The Revelate Engine
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameManagerSubsystem.generated.h"
+
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	DayOne   UMETA(DisplayName="Day One"),
+	DayTwo   UMETA(DisplayName="Day Two"),
+	DayThree UMETA(DisplayName="Day Three"),
+	DayFour  UMETA(DisplayName="Day Four"),
+	DayFive  UMETA(DisplayName="Day Five"),
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewState);
+
+UCLASS(BlueprintType)
+class THEREVELATEENGINE_API UGameManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable) EGameState GetCurrentState() const;
+	UFUNCTION() void HandleGameStateChange(EGameState OldState);
+
+	UPROPERTY(SaveGame) EGameState CurrentGameState;
+	UPROPERTY(SaveGame) EGameState CurrentPlayerState;
+	UPROPERTY(BlueprintReadWrite) FOnGameStateChanged OnGameStateChanged;
+};
