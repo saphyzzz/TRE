f26a348b491873bfaeae27cef05162e660ff90c8
Avalon Brathwaite <virlinthejammer@gmail.com>
2026-03-17 00:29:14 +0000

    Audio, NPC's, Interactions Day Four and Three, Chapel Trigger, Intro audio
    StatesTrigger -> Doesn't complete task unless day three
    Desktop -> Began implementing pop up box for intro text
    GameInstance -> Saves if player first time in game, added this to Bp_SaveGame struct and a function in the interface
    ItemsTrigger -> Now has condition for day four
    ChapelTrigger -> Audio change
    CodeEditor Widgets -> Implemented audio/ changed
    NPC Assets -> NPC's are now in game, rotate, can have discussion with them, freeze movement and rotates player towards them, each can have different sound + text passed in, made UI widgets for this as well.
    Audio Assets
    TaskManager -> Keeps track of the NPC and items tasks for Day four

--- Source/TheRevelateEngine/Managers/GameManagerSubsystem.cpp
+++ Source/TheRevelateEngine/Managers/GameManagerSubsystem.cpp
@@ -45,13 +45,13 @@ EGameState UGameManagerSubsystem::SetCurrentGameState(EGameState SavedState, boo
 			NextGameState = static_cast<EGameState>(NextState);
 		}
 		// DEBUG
-		NextGameState = EGameState::DayThree;
+		NextGameState = EGameState::DayFour;
 		// DEBUG
 		CurrentGameState = NextGameState;
 		return NextGameState;
 	}  
 	// DEBUG
-	SavedState = EGameState::DayThree;
+	SavedState = EGameState::DayFour;
 	// DEBUG
 	CurrentGameState = SavedState;
 	return SavedState;
