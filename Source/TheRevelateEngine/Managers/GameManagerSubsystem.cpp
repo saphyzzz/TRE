2a5bb7655ad73ccb342e2232b153841bc6a83fbf
Avalon Brathwaite <virlinthejammer@gmail.com>
2026-03-24 00:10:08 +0000

    AudioMixer sliders work, added audio for level editor, Day 5, Win API
    - If State > 5 then windows pop up, pressing okay quits game
    - NPC scare on day five ->  launches towards player
    - Flickering lights on PC's for day five same with lights
    - HUD disables movement if day 5 until dialogue ends
    - Booting off animation
    - Escape button for desktop

--- Source/TheRevelateEngine/Managers/GameManagerSubsystem.cpp
+++ Source/TheRevelateEngine/Managers/GameManagerSubsystem.cpp
@@ -3,6 +3,8 @@
 
 #include "GameManagerSubsystem.h"
 
+#include <iostream>
+
 #include "EngineUtils.h"
 #include "ImageUtils.h"
 
@@ -20,33 +22,32 @@ void UGameManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	
 	Super::Initialize(Collection);
 	HandleWallpaper();
-	// have a check of the current state if it is already five and maybe if the cinematic has been reached then do the black screen with its been done text or something idk.  
 }
-//
-// void UGameManagerSubsystem::Deinitialize()
-// {
-// 	Super::Deinitialize();
-// }
 
 EGameState UGameManagerSubsystem::SetCurrentGameState(EGameState SavedState, bool bTasksCompleted)
 {
+	// Create pop up window if player has finished all Days, they can no longer enter the game 
+	if (static_cast<int>(SavedState) >= static_cast<int>(EGameState::DayFive))
+	{
+		LPCWSTR Content = L"Initiation completed, We will call in time.";
+		int Code = MessageBox(NULL, Content, L"Revelate Corporation", MB_OK);
+
+		if (Code == IDOK)
+		{
+			FGenericPlatformMisc::RequestExit(false);
+		}
+	}
 	if (bTasksCompleted)
 	{
 		EGameState NextGameState;
 		int NextState = static_cast<int>(SavedState) + 1;
-		int Entries = StaticEnum<EGameState>()->NumEnums() - 1;
-		if (NextState >= Entries)
-		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Max State reached"));
-			NextGameState = SavedState;
-		}
-		else
-		{
-			NextGameState = static_cast<EGameState>(NextState);
-		}
+		NextGameState = static_cast<EGameState>(NextState);
+		
+		// }
 		// DEBUG
 		NextGameState = EGameState::DayFive;
 		// DEBUG
+		
 		CurrentGameState = NextGameState;
 		return NextGameState;
 	}  
